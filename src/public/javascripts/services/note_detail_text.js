af03abbd6828840f8dc826b5e77925be9721c575
vitogit <vitogit@users.noreply.github.com>
2019-02-09 13:31:27 -0300

    Orginize code a little

--- src/public/javascripts/services/note_detail_text.js
+++ src/public/javascripts/services/note_detail_text.js
@@ -7,51 +7,14 @@ const $component = $('#note-detail-text');
 
 let textEditor = null;
 
-// TODO Prosemirror plugins, move them to another place
-let statsPlugin = new Plugin({
-  state: {
-    init(_, {doc}) { return showStats(doc.textContent) },
-    apply(tr, old) { return tr.docChanged ? showStats(tr.doc.textContent) : old }
-  }
-})
-
-let savePlugin = new Plugin({
-  state: {
-    init(_, {doc}) { return console.log('loaded') },
-    apply(tr, old) { return tr.docChanged ? noteDetailService.noteChanged() : old }
-  }
-})
-
 async function show() {
-    // onNoteChange(noteDetailService.noteChanged);
     // isReadOnly = await isReadOnly();
-
     let content = noteDetailService.getCurrentNote().content
-    let parsedContent, doc
-    
-    // TODO This is to support both html and parsed json. Just to test with the current data.
-    // In the future will be all json parsed and serialized
-    try {
-      parsedContent = JSON.parse(noteDetailService.getCurrentNote().content)
-      doc = mySchema.nodeFromJSON(parsedContent)
-    } catch (e) {
-      const element = document.createElement('div')
-      element.innerHTML = content.trim()
-      $('<div/>').html(content.trim())
-      doc = DOMParser.fromSchema(mySchema).parse(element)
-    }
-    
-    // TODO improve the way to load the doc
-    let state = EditorState.create({
-      doc: doc,
-      // editable: !isReadOnly,
-      plugins: exampleSetup({schema: mySchema}).concat(newKeymap).concat(savePlugin).concat(statsPlugin)
-    })
-    window.prosemirrorview.updateState(state)
+    window.prosemirrorview.loadContent(content)
 }
 
 function getContent() {
-    return JSON.stringify(window.prosemirrorview.state.doc.toJSON());
+    return window.prosemirrorview.getContent();
 }
 
 async function isReadOnly() {
@@ -66,10 +29,6 @@ function focus() {
 
 function getEditor() {
     return textEditor;
-}
-
-function onNoteChange(func) {
-    // textEditor.model.document.on('change:data', func);
 }
 
 $component.on("dblclick", "img", e => {
@@ -93,7 +52,6 @@ export default {
     getEditor,
     getContent,
     focus,
-    onNoteChange,
     cleanup: () => {
         if (textEditor) {
             textEditor.setData('');
--- src/public/libraries/prosemirror.js
+++ src/public/libraries/prosemirror.js
@@ -4,10 +4,25 @@ const {Schema, DOMParser} = require("prosemirror-model")
 const {schema} = require("prosemirror-schema-basic")
 const {addListNodes, sinkListItem, liftListItem, splitListItem} = require("prosemirror-schema-list")
 const {exampleSetup} = require("prosemirror-example-setup")
-  const {keymap} = require("prosemirror-keymap")
+const {keymap} = require("prosemirror-keymap")
 
+function showStats(text) {
+  console.log("text.length________",text.length)
+}
 
+let statsPlugin = new Plugin({
+  state: {
+    init(_, {doc}) { return showStats(doc.textContent) },
+    apply(tr, old) { return tr.docChanged ? showStats(tr.doc.textContent) : old }
+  }
+})
 
+let savePlugin = new Plugin({
+  state: {
+    init(_, {doc}) { return console.log('loaded') },
+    apply(tr, old) { return tr.docChanged ? window.glob.noteChanged() : old }
+  }
+})
 
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
@@ -21,6 +36,30 @@ let newKeymap = keymap({
   'Tab': sinkListItem(mySchema.nodes.list_item)
 })
 
+function loadContent( content ) {
+    let parsedContent, doc
+
+    // TODO This is to support both html and parsed json. Just to test with the current data.
+    // In the future will be all json parsed and serialized
+    try {
+      parsedContent = JSON.parse(content)
+      doc = mySchema.nodeFromJSON(parsedContent)
+    } catch (e) {
+      const element = document.createElement('div')
+      element.innerHTML = content.trim()
+      $('<div/>').html(content.trim())
+      doc = DOMParser.fromSchema(mySchema).parse(element)
+    }
+
+    // TODO improve the way to load the doc
+    let state = EditorState.create({
+      doc: doc,
+      // editable: !isReadOnly,
+      plugins: exampleSetup({schema: mySchema}).concat(newKeymap).concat(statsPlugin).concat(savePlugin)
+    })
+    window.prosemirrorview.updateState(state)
+}
+
 window.prosemirrorview = new EditorView(document.querySelector("#note-detail-text"), {
   state: EditorState.create({
     doc: DOMParser.fromSchema(mySchema).parse(""),
@@ -28,6 +67,9 @@ window.prosemirrorview = new EditorView(document.querySelector("#note-detail-tex
   })
 })
 
-function showStats(text) {
-  console.log("text.length________",text.length)
+prosemirrorview.loadContent = loadContent
+
+prosemirrorview.getContent = function() {
+  console.log("getcontent")
+  return JSON.stringify(window.prosemirrorview.state.doc.toJSON());
 }
